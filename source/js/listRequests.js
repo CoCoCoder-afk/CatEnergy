8e69711b9697b6378a3fe2c87644bd51371bb6b7
Graudeep <grisha.perevalov@gmail.com>
2020-07-23 16:18:24 +0300

    Сделал удаление со стороны бэкенда, но сломал со стороны фронта

--- source/js/listRequests.js
+++ source/js/listRequests.js
@@ -15,6 +15,7 @@ let database = firebase.database(),
     data;
 
 let content = "";
+let items = "";
 
 function signOut() {
 
@@ -22,19 +23,40 @@ function signOut() {
     window.location.replace("index.html");
 
 }
+let someObj = {
+    key: '1',
+    value: '1',
+    sign: 'equal',
+}
 let flag = true;
+let numOf = 0;
+let keys = [];
 
 ref.on("value", function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
-        data = childSnapshot.val(); 
-        content += `<div class="items__item item"><hr><p class="item__text"><span id="userName">${data.name}</span><br><span id="userAge">${data.age}</span><br><span id="userWeight">${data.weight}</span><br><span id="userEmail">${data.email}</span><br><span id="userPhone">${data.phone}</span><br><span id="userComment">${data.comment}</span><br><span id="userSweetener">${data.sweetener}</span><br><span id="userWater">${data.water}</span><br><span id="userMilk">${data.milk}</span><br><span id="userVitamins">${data.vitamins}</span></p><p class="item__sub">true - user needs / false - user don't<button onclick="requestDelete()" id="deleteBtn" class="item__delete">Delete</button></p><hr></div>`
+        data = childSnapshot.val();
+        keys.push(childSnapshot.key);
+        content += `<div class="items__item item"><hr><p class="item__text"><span id="userName">${data.name}</span><br><span id="userAge">${data.age}</span><br><span id="userWeight">${data.weight}</span><br><span id="userEmail">${data.email}</span><br><span id="userPhone">${data.phone}</span><br><span id="userComment">${data.comment}</span><br><span id="userSweetener">${data.sweetener}</span><br><span id="userWater">${data.water}</span><br><span id="userMilk">${data.milk}</span><br><span id="userVitamins">${data.vitamins}</span></p><p class="item__sub">true - user needs / false - user don't<button id="deleteBtn" class="item__delete">Delete</button></p><p class = "idNum">${numOf}</p><hr></div>`;
     });
-    let items = document.querySelector('#items');
+    items = document.querySelector('#items');
     items.insertAdjacentHTML('afterEnd', content);
+    let buttonsRemove = document.querySelectorAll(".item__delete");
+    for (let i = 0; i < keys.length; i++) {
+        buttonsRemove[i].addEventListener("click", function () {
+            removeRequest(keys[i])
+        }, false);
+    }
 })
 
-function requestDelete() {
-    document.querySelector(".item").innerHTML = "";
-    alert("You have successfully deleted CatEnergy request.");
-    data.removeValue();
+function removeRequest(key) {
+    alert("Successfully done");
+    ref.child(key).remove();
+}
+
+if (items == "") {
+    items = "<h3>ќжидаем за€вки</h3>";
+}
+
+function renderRequest(doc) {
+    document.querySelector(".item").setAttribute("data-id", doc.id);
 }
