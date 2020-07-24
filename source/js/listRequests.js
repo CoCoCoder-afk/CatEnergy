e163743e38aff30756fe27182476f47f68bf3cf3
Graudeep <grisha.perevalov@gmail.com>
2020-07-24 19:28:59 +0300

    Сделал, чтобы при удалении со стороны сервера происходило удаление со стороны фронтенда без перезапуска страницы, хотя и понадобился для этого небольшой костыль

--- source/js/listRequests.js
+++ source/js/listRequests.js
@@ -15,7 +15,6 @@ let database = firebase.database(),
     data;
 
 let content = "";
-let items = "";
 
 function signOut() {
 
@@ -23,40 +22,43 @@ function signOut() {
     window.location.replace("index.html");
 
 }
-let someObj = {
-    key: '1',
-    value: '1',
-    sign: 'equal',
-}
-let flag = true;
-let numOf = 0;
 let keys = [];
+let child_removed = false;
 
-ref.on("value", function (snapshot) {
-    snapshot.forEach(function (childSnapshot) {
-        data = childSnapshot.val();
-        keys.push(childSnapshot.key);
-        content += `<div class="items__item item"><hr><p class="item__text"><span id="userName">${data.name}</span><br><span id="userAge">${data.age}</span><br><span id="userWeight">${data.weight}</span><br><span id="userEmail">${data.email}</span><br><span id="userPhone">${data.phone}</span><br><span id="userComment">${data.comment}</span><br><span id="userSweetener">${data.sweetener}</span><br><span id="userWater">${data.water}</span><br><span id="userMilk">${data.milk}</span><br><span id="userVitamins">${data.vitamins}</span></p><p class="item__sub">true - user needs / false - user don't<button id="deleteBtn" class="item__delete">Delete</button></p><p class = "idNum">${numOf}</p><hr></div>`;
-    });
-    items = document.querySelector('#items');
-    items.insertAdjacentHTML('afterEnd', content);
-    let buttonsRemove = document.querySelectorAll(".item__delete");
-    for (let i = 0; i < keys.length; i++) {
-        buttonsRemove[i].addEventListener("click", function () {
-            removeRequest(keys[i])
-        }, false);
-    }
+ref.on("child_removed", function () {
+    child_removed = true;
 })
 
+ref.on("value", function (snapshot) {
+    if (!child_removed) {
+        snapshot.forEach(function (childSnapshot) {
+            data = childSnapshot.val();
+            keys.push(childSnapshot.key);
+            content += `<div class="items__item item"><hr><p class="item__text"><span id="userName">${data.name}</span><br><span id="userAge">${data.age}</span><br><span id="userWeight">${data.weight}</span><br><span id="userEmail">${data.email}</span><br><span id="userPhone">${data.phone}</span><br><span id="userComment">${data.comment}</span><br><span id="userSweetener">${data.sweetener}</span><br><span id="userWater">${data.water}</span><br><span id="userMilk">${data.milk}</span><br><span id="userVitamins">${data.vitamins}</span></p><p class="item__sub">true - user needs / false - user don't<button id="deleteBtn" class="item__delete">Delete</button></p><hr></div>`;
+        });
+        items = document.querySelector('#items');
+        items.insertAdjacentHTML('afterEnd', content);
+        let buttonsRemove = document.querySelectorAll(".item__delete");
+        for (let i = 0; i < keys.length; i++) {
+            buttonsRemove[i].addEventListener("click", removeRequest.bind(buttonsRemove[i], keys[i]), false);
+        }
+    }
+    else {
+        console.log("Хуй тебе, вот такой");
+    }
+});
+
 function removeRequest(key) {
-    alert("Successfully done");
+
+    this.parentNode.parentNode.remove();
     ref.child(key).remove();
+
 }
 
-if (items == "") {
-    items = "<h3>Ожидаем заявки</h3>";
-}
+
 
 function renderRequest(doc) {
     document.querySelector(".item").setAttribute("data-id", doc.id);
 }
+
+
